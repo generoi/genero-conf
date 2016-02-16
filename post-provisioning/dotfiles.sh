e276414d08bc33161fa2631afab49a98cd8040cd
Oskar Schöldström <public@oxy.fi>
2016-02-16 09:00:02 -0500

    fix dotfiles post provisioning script

--- post-provisioning/dotfiles.sh
+++ post-provisioning/dotfiles.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-su vagrant
-
-DOTFILES_DOWNLOAD=https://github.com/generoi/dotfiles/archive/master.tar.gz
-DOTFILES_DOWNLOAD_DIR=~/dotfiles
+USERNAME=vagrant
+DOTFILES_GIT=https://github.com/generoi/dotfiles.git
+DOTFILES_DOWNLOAD_DIR=/home/$USERNAME/dotfiles
 
 # Check to see if we've already performed this setup.
-if [ ! -d "$DOTFILES_DOWNLOAD_DIR/bootstrap.sh" ]; then
+if [ ! -e "$DOTFILES_DOWNLOAD_DIR/bootstrap.sh" ]; then
   # Download and expand the dotfiles
-  mkdir -p $DOTFILES_DOWNLOAD_DIR
-  wget -qO- $DOTFILES_DOWNLOAD | tar xvz --strip 1 -C $DOTFILES_DOWNLOAD_DIR
+  sudo -u $USERNAME -H git clone $DOTFILES_GIT $DOTFILES_DOWNLOAD_DIR
 
   # Install the dotfiles.
-  cd $DOTFILES_DOWNLOAD_DIR
-  ./bootstrap.sh
+  sudo -u $USERNAME -H sh -c "cd $DOTFILES_DOWNLOAD_DIR; bash bootstrap.sh"
 else
   exit 0
 fi
