897ed8653e46c2c299fd6021682a68f64eb3201d
Oskar Schöldström <public@oxy.fi>
2016-05-21 17:42:46 -0500

    Generate unique hash salt file for drupal installations

--- post-provisioning/drupal.sh
+++ post-provisioning/drupal.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+ROOT_PATH=/var/www/drupal/web
+
 FILES_USER=vagrant
 FILES_GROUP=www-data
-ROOT_PATH=/var/www/drupal/web
 FILES_PATH=${ROOT_PATH}/sites/default/files
 
 # Ensure the files directory has the correct permissions.
@@ -10,3 +11,12 @@ sudo mkdir -p $FILES_PATH
 sudo chmod 775 $FILES_PATH
 sudo chown -R $FILES_USER:$FILES_GROUP $FILES_PATH
 sudo chmod g+s $FILES_PATH
+unset FILES_PATH FILES_USER FILES_GROUP
+
+# Generate a unique hash salt.
+HASH_SALT_FILE=${ROOT_PATH}/../config/salt.txt
+if [ ! -e "$HASH_SALT_FILE" ]; then
+  SALT="$(openssl rand -base64 32)"
+  echo "$SALT" > "$HASH_SALT_FILE"
+fi
+unset HASH_SALT_FILE SALT
