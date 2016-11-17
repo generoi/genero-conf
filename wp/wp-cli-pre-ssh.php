6c4bcd4241f12a6adb526bd9af28f6cc71faa614
Oskar Schöldström <public@oxy.fi>
2016-11-17 11:28:59 -0500

    Add wp-cli script which adds vendor bins to $PATH

---
+++ wp/wp-cli-pre-ssh.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Add vendor binaries to $PATH so that wp-cli is available on the remote host
+ * when it's a composer dependency.
+ *
+ * @see https://runcommand.io/to/wp-ssh-custom-path/
+ */
+WP_CLI::add_hook('before_ssh', function() {
+  // Eg. /var/www/wordpress/web/wp
+  $wp_path = WP_CLI\Utils\parse_ssh_url(WP_CLI::get_runner()->config['ssh'], PHP_URL_PATH);
+  // Eg. /var/www/wordpress
+  $project_root = dirname(dirname($wp_path));
+  // Eg. /var/www/wordpress/vendor/bin
+  $vendor_path = "$project_root/vendor/bin";
+  // Additionally add the users globally installed composer binaries.
+  $composer_path = '$HOME/.composer/vendor/bin';
+  putenv('WP_CLI_SSH_PRE_CMD=export PATH=' . $vendor_path . ':' . $composer_path . ':$PATH');
+});
